a94331433ea21a0d719216ac8473e706166d6004
tom <thomas@grozev.com.au>
2020-11-08 15:45:43 +1100

    Origin fix

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :badges, BadgesWeb.Endpoint,
   root: ".",
   version: Application.spec(:badges, :vsn),
   load_from_system_env: true,
-  check_origin: [System.get_env("HOST")],
+  check_origin: [System.get_env("ORIGIN_HOST")],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
